5697dcc6dc382227c754b55c3eba52f6b08885fc
Satvik Yadav <satvikydv@gmail.com>
2024-07-01 00:07:06 +0530

    bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import {v2 as cloudinary} from "cloudinary";
-import fs from "fs";    //filesystem
+import fs, { unlink } from "fs";    //filesystem
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto",
         })
         //file is now uploaded on cloudinary
-        console.log("File uploaded successfully on cloudinary ", response.url);
+        // console.log("File uploaded successfully on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath);    //remove the locally saved temporary file
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath);    //remove the locally saved temporary file as the upload operation failed
